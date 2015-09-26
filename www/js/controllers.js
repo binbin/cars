f04dbd5a76120c2a3d747661cd05513d5a8f5f91
binbin <binbin5474@gmail.com>
2015-09-26 14:05:08 +0800

    active

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,27 +8,40 @@ angular.module('cars.controller', [])
 		level:'C',
                         tel:'13708357405',
                         trend_type:'长安汽车长安CS752014款 1.8T 自动精英型 国IV',
-                        tel_times:10
+                        tel_times:10,
+                        customer_type:1
 	},{
 		name:'李四',
 		level:'A',
                         tel:'33708357405',
                         trend_type:'长安汽车长安CS752014款 1.8T 自动精英型 国IV',
-                        tel_times:10
+                        tel_times:10,
+                         customer_type:0
 	},{
 		name:'王二',
 		level:'B',
                         tel:'443708357405',
                         trend_type:'长安汽车长安CS752014款 1.8T 自动精英型 国IV',
-                        tel_times:10
+                        tel_times:10,
+                        customer_type:0
 	},{
 		name:'赵五',
 		level:'H',
                         tel:'43708357405',
                         trend_type:'长安汽车长安CS752014款 1.8T 自动精英型 国IV',
-                        tel_times:10
+                        tel_times:10,
+                        customer_type:2
 	}]
 	$scope.customers=customers
+
+	$scope.type=0;
+
+	$scope.setType=function(event,type){
+		$scope.type=type;
+		$scope.customers = customers.filter(function(item){
+                              return item.customer_type === type
+		})
+	}
 })
 .controller('EmployeeCtrl', function($scope) {})
 .controller('GradeCtrl', function($scope) {})
